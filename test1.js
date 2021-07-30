f7a8420275e7cee6debc7bce24addf7b5a48ce4a
unknown <fj2008@naver.com>
2021-07-30 15:25:39 +0900

    ajax 비동기프로그래밍 예제

---
+++ test1.js
@@ -0,0 +1,14 @@
+
+console.log("코드1");
+hello();
+
+console.log("코드2");
+console.log("코드3");
+
+for(var i=0; i<100000000000; i++){
+    console.log("코드4");
+}
+
+async function hello(){
+    let num = await fetch("http://fasdlfkjalsdf.com");
+}
